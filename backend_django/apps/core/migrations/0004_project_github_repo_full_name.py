62e1271e0f76c22d8fe8d4e3db881c3eae71c03c
Christian <A01231399@tec.mx>
2026-04-13 17:47:27 -0600

    feat: modify migration to add github_repo_full_name column with SQL command

--- backend_django/apps/core/migrations/0004_project_github_repo_full_name.py
+++ backend_django/apps/core/migrations/0004_project_github_repo_full_name.py
@@ -8,9 +8,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="project",
-            name="github_repo_full_name",
-            field=models.CharField(blank=True, db_index=True, max_length=255, null=True),
+        migrations.RunSQL(
+            sql="ALTER TABLE project ADD COLUMN IF NOT EXISTS github_repo_full_name VARCHAR(255);",
+            reverse_sql="ALTER TABLE project DROP COLUMN IF EXISTS github_repo_full_name;",
+        ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="project",
+                    name="github_repo_full_name",
+                    field=models.CharField(blank=True, db_index=True, max_length=255, null=True),
+                ),
+            ],
+            database_operations=[],
         ),
     ]
