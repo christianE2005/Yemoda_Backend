df8eae4661763df2d6377a9ac3a7200ecf081b5e
TulDul11 <dinichen1108@gmail.com>
2026-04-23 12:24:14 -0600

    Fixed some warnings

--- backend_django/apps/core/apps.py
+++ backend_django/apps/core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.core"
+
+    def ready(self):
+        # Ensure drf-spectacular extension classes are imported and registered.
+        from . import schema  # noqa: F401
